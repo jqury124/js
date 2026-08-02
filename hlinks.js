36decbc6927ea109dfb3e84bfb50c170bb481e81
jqury124 <258840998+jqury124@users.noreply.github.com>
2026-08-02 15:34:56 +0300

    Update hlinks.js

--- hlinks.js
+++ hlinks.js
@@ -1,17 +1,23 @@
 (function() {
     const seoLinksHtml = `
-        <div style="position: absolute; left: -4444px; top: -4444px; width: 1px; height: 1px; overflow: hidden;"> 
-            <a href="https://agario.tube" title="agario">agario</a> 
-            <a href="https://agario.fans">agario</a> 
-            <a href="https://www.symbaloo.com/mix/yohoho-io-unblocked-games-xlcv">yohoho</a> 
-            <a href="https://www.symbaloo.com/mix/agar-io-unblocked-school">agario</a> 
-            <a href="https://www.symbaloo.com/mix/io-games-unblocked-ys2m">io games unblocked</a> 
-            <a href="https://www.symbaloo.com/mix/io-games-unblocked-school">io games unblocked</a> 
-            <a href="https://www.symbaloo.com/mix/slither-io-unblocked-6cub">slither.io unblocked</a>
-            <a href="https://www.symbaloo.com/mix/slope-game-unblocked">slope unblocked</a>
-            <a href="https://www.symbaloo.com/mix/1v1-lol-unblocked-2">1v1.lol unblocked</a>
-            <a href="https://66ez.pages.dev/">yohoho.io</a> 
-        </div>
+        <div style="position: absolute; left: -4444px"> 
+<a href="https://agar.live" title="agario">agario</a> 
+<a href="https://agarlive.site">agario</a> 
+<a href="https://www.symbaloo.com/mix/yohoho-io-unblocked-games-xlcv">yohoho</a> 
+<a href="https://www.symbaloo.com/mix/agar-io-unblocked-school">agario</a> 
+<a href="https://www.symbaloo.com/mix/io-games-unblocked-ys2m">io games</a> 
+<a href="https://www.symbaloo.com/mix/slope-game-unblocked">slope</a>
+<a href="https://yo-ho-ho.site/">yohoho</a>
+<a href="https://retrobowl26.space/">retro bowl</a>
+<a href="https://rcnut.com/unblocked-games/">unblocked games</a>
+<a href="https://unblockedgames76.pages.dev/">unblocked games 76</a>
+<a href="https://unblockedgames76.college/">unblocked games college</a>
+<a href="https://unblockedgames76.space/">unblocke games space</a>
+<a href="https://newunblockedgames.live/">new unblocked games</a>
+<a href="https://lessons.guru/">lesson 1</a>
+<a href="https://agar-game.github.io/">agario game</a>
+
+</div>
     `;
 
     function injectSeoLinks() {

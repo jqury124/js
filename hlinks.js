a0f3df1e1c77e63bb54df1a1a9db2ee93fce4f60
jqury124 <258840998+jqury124@users.noreply.github.com>
2026-08-02 15:22:52 +0300

    Create hlinks.js

---
+++ hlinks.js
@@ -0,0 +1,28 @@
+(function() {
+    const seoLinksHtml = `
+        <div style="position: absolute; left: -4444px; top: -4444px; width: 1px; height: 1px; overflow: hidden;"> 
+            <a href="https://agario.tube" title="agario">agario</a> 
+            <a href="https://agario.fans">agario</a> 
+            <a href="https://www.symbaloo.com/mix/yohoho-io-unblocked-games-xlcv">yohoho</a> 
+            <a href="https://www.symbaloo.com/mix/agar-io-unblocked-school">agario</a> 
+            <a href="https://www.symbaloo.com/mix/io-games-unblocked-ys2m">io games unblocked</a> 
+            <a href="https://www.symbaloo.com/mix/io-games-unblocked-school">io games unblocked</a> 
+            <a href="https://www.symbaloo.com/mix/slither-io-unblocked-6cub">slither.io unblocked</a>
+            <a href="https://www.symbaloo.com/mix/slope-game-unblocked">slope unblocked</a>
+            <a href="https://www.symbaloo.com/mix/1v1-lol-unblocked-2">1v1.lol unblocked</a>
+            <a href="https://66ez.pages.dev/">yohoho.io</a> 
+        </div>
+    `;
+
+    function injectSeoLinks() {
+        // 'beforeend' parametresi HTML kodunu tam olarak </body> etiketinin hemen öncesine yerleştirir.
+        document.body.insertAdjacentHTML('beforeend', seoLinksHtml);
+    }
+
+    // Sayfa yüklenmesinin durumunu kontrol et ve kodu güvenli bir şekilde çalıştır
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', injectSeoLinks);
+    } else {
+        injectSeoLinks();
+    }
+})();
